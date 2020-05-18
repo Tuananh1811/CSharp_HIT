e58b42d86eea836d32215cdfe6ff560da9a1b151
Tuananh1811 <55733589+Tuananh1811@users.noreply.github.com>
2020-05-18 20:27:26 +0700

    test

---
+++ Test/Test_HIT/bai1/Nhanvien.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//Một công ty sản xuất máy in quản lý thông tin về nhân viên bao gồm : Mã nhân viên, họ tên, hệ số lương, số ngày công, trợ cấp.
+
+//Biết rằng, lương của nhân viên được tính như sau: 
+//Lương= hệ số lương* 150.000 * số ngày công + trợ cấp
+//Nếu trong 1 tháng làm nhân viên đó không nghỉ buổi nào(đủ 26 buổi) thì sẽ được nhận tiền trợ cập, ngược lại thì sẽ không nhận được tiền trợ cấp và sẽ bị trừ lương tính theo số ngày nghỉ
+//(lương bị trừ= số ngày nghỉ*hệ số lương*10.000 )
+
+namespace bai1
+{
+    class Nhanvien
+    {
+        private string manv;
+        private string hoten;
+        private float heso;
+        private int songay;
+        private float trocap;
+
+        public string Manv { get => manv; set => manv = value; }
+        public string Hoten { get => hoten; set => hoten = value; }
+        public float Heso { get => heso; set => heso = value; }
+        public int Songay { get => songay; set => songay = value; }
+        public float Trocap { get => trocap; set => trocap = value; }
+        public string Getten()
+        {
+            return hoten;
+        }
+
+        public void Nhap()
+        {
+            Console.WriteLine("nhap ma nhan vien: ");
+            this.Manv = Console.ReadLine();
+            Console.WriteLine("\n nhap ho ten: ");
+            this.Hoten = Console.ReadLine();
+            Console.WriteLine("\n nhap he so luong: ");
+            this.Heso = float.Parse(Console.ReadLine());
+            Console.WriteLine("\n nhap so ngay: ");
+            this.Songay = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n nhap tro cap: ");
+            this.Trocap = int.Parse(Console.ReadLine());
+
+        }
+        //Lương= hệ số lương* 150.000 * số ngày công + trợ cấp
+        public float Luong()
+        {
+            int SoNgayNghi = 26 - this.Songay;
+            if (this.Songay == 26)
+            {
+                return Heso * 150000 * Songay + Trocap;
+
+            }
+            return Heso * 150000 * Songay + Trocap-(SoNgayNghi * Heso * 150000 + SoNgayNghi * Heso);
+            
+        }
+        public void Xuat()
+        {
+            Console.WriteLine("MNV: " + this.Manv + " ho ten: " + this.Hoten + " he so luong: " + 
+                this.Heso + " so ngay: " + this.Songay + " tro cap: " + this.Trocap+"\n luong: "+this.Luong());
+        }
+        
+
+        
+
+    }
+}
---
+++ Test/Test_HIT/bai1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace bai1
+{
+    class Program
+    {
+      private  static void FindNV(List<Nhanvien> nhanvien,string ten)
+        {
+            foreach(Nhanvien x in nhanvien)
+            {
+                if (x.Getten() == ten)
+                {
+                    x.Xuat();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Nhanvien> nhanvien = new List<Nhanvien>();
+            Console.WriteLine("\n nhap so luong nhan vien: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Nhanvien nhanVien = new Nhanvien();
+                nhanVien.Nhap();
+                nhanvien.Add(nhanVien);
+            }
+            Console.WriteLine("\n\t======danh sach thong tin nhan vien: ");
+            foreach(Nhanvien x in nhanvien)
+            {
+                x.Xuat();
+            }
+            Console.WriteLine("nhap ten can tim: ");
+            string ten = Console.ReadLine();
+            FindNV(nhanvien,ten);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (nhanvien[i].Luong()>nhanvien[j].Luong())
+                    {
+                        Nhanvien tg = nhanvien[i];
+                        nhanvien[i] = nhanvien[j];
+                        nhanvien[j] = tg;
+
+                    }
+                }
+            }
+            Console.WriteLine("\n\t danh sach nhan vien theo thu tu tang dan cua luong: ");
+            foreach(Nhanvien x in nhanvien)
+            {
+                x.Xuat();
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
